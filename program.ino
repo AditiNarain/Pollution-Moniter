e175ed4caf827432ade996f20fbe13f55cc1ab3d
Aditi <aditinarain.60@gmail.com>
2021-01-15 20:01:41 +0530

    Arduino IDE Program

--- program.ino
+++ program.ino
@@ -32,12 +32,12 @@ void loop()
   lcd.print(gas_val);
   lcd.print(" PPM");
   lcd.setCursor (1,1);
-   if (gas_val<=650) //if the air quality is less than or equal to 650 ppm
+   if (gas_val<=660) //if the air quality is less than or equal to 660 ppm
    {
    lcd.print("Fresh Air");
    Serial.println("Fresh Air ");
   }
-  else if( gas_val>650 && gas_val<=900 ) //if the air quality is more than 650 ppm less than 900 ppm
+  else if( gas_val>660 && gas_val<=900 ) //if the air quality is more than 660 ppm less than 900 ppm
   {
    lcd.print(" Poor Air");
    Serial.println(" Poor Air");
